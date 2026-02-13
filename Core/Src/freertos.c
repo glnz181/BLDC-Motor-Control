4399941b8f48acbd45ac2a0a72508850be9bf9e9
glnz181 <sekerirem58@gmail.com>
2026-02-13 08:43:49 +0300

    Sadece staj5 motor projesi yukleniyor

---
+++ Core/Src/freertos.c
@@ -0,0 +1,211 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * File Name          : freertos.c
+ * Description        : Code for freertos applications
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define EC_SERVICE_ENABLE  0x00
+#define TARGET_AXIS_ID     0x01
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+extern CAN_HandleTypeDef hcan1;
+extern UART_HandleTypeDef huart1;
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 216 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for myTask02 */
+osThreadId_t myTask02Handle;
+const osThreadAttr_t myTask02_attributes = {
+  .name = "myTask02",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+void StartTask02(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+	/* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+	/* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+	/* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+	/* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of myTask02 */
+  myTask02Handle = osThreadNew(StartTask02, NULL, &myTask02_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+	/* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+	/* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+	/* USER CODE BEGIN 5 */
+	CAN_RxHeaderTypeDef RxHeader;
+	uint8_t RxData[8];
+	/* Infinite loop */
+	for (;;) {
+		if (HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &RxHeader, RxData)
+				== HAL_OK) {
+			// Gelen mesajın Status bilgisini kontrol et
+			if (RxData[0] == 0x00) { // ACK_ACCEPTED [cite: 198]
+				// Komut başarıyla alındı
+			} else if (RxData[0] == 0x01) { // ACK_ERROR [cite: 198]
+				// Bir hata var!
+			}
+		}
+		osDelay(1);
+	}
+
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_StartTask02 */
+/**
+ * @brief Function implementing the myTask02 thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartTask02 */
+void StartTask02(void *argument)
+{
+  /* USER CODE BEGIN StartTask02 */
+	CAN_TxHeaderTypeDef TxHeader;
+	uint8_t TxData[8];
+	uint32_t TxMailbox;
+
+	// 1. Değişkenleri Tanımla
+	uint32_t priority = 7;      // [cite: 178]
+	uint32_t service_bit = 1;   // Service mesajı olduğu için 1 [cite: 179]
+	uint32_t request_bit = 1;   // İstek (Request) olduğu için 1 [cite: 181]
+	uint32_t service_id = 0x00; // Enable/Disable Servis ID'si [cite: 206]
+	uint32_t axis_bit = 1;  // Tek eksene (Axis) gönderildiği için 1 [cite: 183]
+	uint32_t dest_id = 1;       // Hedef Motor ID (TARGET_AXIS_ID) [cite: 184]
+	uint32_t source_id = 0;     // Gönderen (STM32) ID'si [cite: 187]
+
+	// 2. CAN Header Ayarlarını Yap
+	TxHeader.IDE = CAN_ID_EXT;
+	TxHeader.RTR = CAN_RTR_DATA;
+	TxHeader.TransmitGlobalTime = DISABLE;
+	TxHeader.DLC = 1; // Enable komutu 1 byte veri ister [cite: 206]
+
+	// 3. ExtId Hesapla (Değişkenleri burada kullanıyoruz)
+	// Dökümandaki bit dizilimine göre (Figure 5) [cite: 177]
+	TxHeader.ExtId = (priority << 26) | (service_bit << 25)
+			| (request_bit << 24) | (service_id << 16) | (axis_bit << 15)
+			| (dest_id << 8) | (source_id);
+
+	// 4. Veriyi Hazırla (Enable komutu için 0x01)
+	TxData[0] = 0x01; // CMD = 0x01 (Enable) [cite: 208]
+
+	/* Infinite loop */
+	for (;;) {
+		// 5. Mesajı Gönder
+		if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox)
+				== HAL_OK) {
+			HAL_UART_Transmit(&huart1, (uint8_t*) "CAN: Enable Yollandi\r\n",
+					22, 100);
+		} else {
+			// Mailbox doluysa veya hat meşgulse buraya girer
+			HAL_UART_Transmit(&huart1,
+					(uint8_t*) "HATA: Mesaj Gonderilemedi!\r\n", 28, 100);
+		}
+		osDelay(1000);
+	}
+
+  /* USER CODE END StartTask02 */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
